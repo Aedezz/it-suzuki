ed9c90c09936f2a79712979bc213fade9bc38289
Aedezz <wafielegend2017@gmail.com>
2024-11-06 11:54:26 +0800

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <h2>Form IT</h2>
+    <br>
+    <div class="row">
+    <div class="col-md-4 text-center">
+    <a herf="#">
+        <i class="#" style="font-size:50 px;"></i>
+        <p style="font-size: 30px;">Install Komputer/Laptop</p>
+    </div>
+</a>
+    </div>
+    <div class="col-md-4 text-center">
+        <a herf="#">
+            <i class="#" style="font-size:50 px;"></i>
+            <p style="font-size: 30px;">Pembuatan User/Reset Password</p>
+        </div>
+    </a>
+        </div>
+        <div class="col-md-4 text-center">
+            <a herf="#">
+                <i class="#" style="font-size:50 px;"></i>
+                <p style="font-size: 30px;">Perbaikan Perangkat</p>
+            </div>
+        </a>    
+            </div>
+</body>
+</html>
