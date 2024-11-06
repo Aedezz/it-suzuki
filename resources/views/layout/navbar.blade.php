2d721d2f09a4a1a8054edf523a7b99327d0128b9
Aedezz <wafielegend2017@gmail.com>
2024-11-06 16:40:29 +0800

    a

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -5,19 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>IT DASH</title>
-    <!-- Include Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <!-- Include Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body bgcolor="#E2F1E7">
-<<<<<<< HEAD
-    <!-- Navbar -->
-    <div class="navbar fixed top-0 left-0 right-0 bg-gray-800 text-white flex items-center p-4 shadow-md z-50">
-=======
-    <!-- Navbar dengan font-sans dari Tailwind -->
     <div class="navbar bg-gray-800 text-white flex items-center p-4">
->>>>>>> 2887851171e8cd895df01b810204312f01e06420
         <div class="navbar-links flex gap-10">
             <a href="/" class="hover:text-gray-400 flex items-center ml-20">
                 <i class="bi bi-house-fill mr-1"></i>
@@ -27,22 +19,11 @@
             <a href="perbaikan" class="hover:text-gray-400 font-sans">Perbaikan Perangkat</a>
             <a href="#" class="hover:text-gray-400 font-sans">Download Berita Acara</a>
         </div>
-<<<<<<< HEAD
-        <!-- Shift the Logo to the Left or Right -->
-=======
->>>>>>> 2887851171e8cd895df01b810204312f01e06420
         <div class="navbar-logo ml-auto mr-10">
             <h2 class="text-xl font-bold">Login IT</h2>
         </div>
     </div>
     
 
-<<<<<<< HEAD
-    <!-- Add padding to avoid content being hidden behind the navbar -->
-    <div class="pt-20">
-        <!-- Rest of the content -->
-    </div>
-=======
->>>>>>> 2887851171e8cd895df01b810204312f01e06420
 </body>
 </html>
