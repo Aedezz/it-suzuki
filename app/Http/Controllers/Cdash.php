bc4bbc14a724bb23a9464de2a9ed48b8c8b92fe8
Aedezz <wafielegend2017@gmail.com>
2024-11-07 15:15:00 +0800

    login

---
+++ app/Http/Controllers/Cdash.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Cdash extends Controller
+{
+    public function index()
+    {
+    	return view('dalam/main');
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Cdash;
+use App\Http\Controllers\Clogin;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Routes for guests (unauthenticated users)
+Route::middleware(['guest'])->group(function () { 
+    Route::get('/', [Clogin::class, 'index'])->name('login'); 
+    Route::post('/', [Clogin::class, 'login_proses'])->name('login_proses'); 
+}); 
+ 
+Route::get('/home', function () { 
+    return redirect('dashboard'); 
+}); 
+ 
+Route::middleware(['auth'])->group(function () { 
+    Route::get('/logout', [Clogin::class, 'logout'])->name('logout'); 
+    Route::get('/main', [Cdash::class, 'index'])->name('main'); 
+    // Route::resource('/siswa', Csiswa::class); 
+}); 
 
-Route::get('/', function () {
-    return view('dashboard');
-});
-
-// routes/web.php
+// Routes for other pages (accessible to authenticated users)
 Route::get('/pembuatan-user', function () {
-    return view('form-db/user'); // Ensure 'user.blade.php' is located in the 'resources/views' folder
+    return view('form-db/user');
 });
-
 Route::get('/installasi-pc', function () {
     return view('form-db/pc');
 });
-
 Route::get('/perbaikan', function () {
     return view('form-db/perbaikan');
 });
-
-// Route::resource()
