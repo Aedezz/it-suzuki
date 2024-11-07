82f0d0a986e02c9f38aba04b8f199850e6843357
Ardhana <firdausdepa.a@gmail.com>
2024-11-07 13:16:54 +0800

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
 </div>
 
 <div style="display: flex; justify-content: center; align-items: center; height: 65vh;">
-    <div style="width: 90%; max-width: 1200px; background-color: #f5f5f5; border-radius: 8px; padding: 50px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); text-align: center; position: relative;">
+    <div style="margin-bottom:-120px ; width: 90%; max-width: 1200px; background-color: #f5f5f5; border-radius: 8px; padding: 50px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); text-align: center; position: relative;">
         <h2 class="font-sans text-2xl font-bold absolute top-4 left-6 form-it-title">Form IT</h2>
 
         <div class="mt-16 flex justify-center gap-10">
