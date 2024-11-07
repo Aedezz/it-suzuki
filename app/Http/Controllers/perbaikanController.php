57131b3c37872a8f43d067c7ef99a5b0f5b77cf5
Ardhana <firdausdepa.a@gmail.com>
2024-11-07 16:11:10 +0800

    update

--- app/Http/Controllers/perbaikanController.php
+++ app/Http/Controllers/perbaikanController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Mperbaikan;
 class perbaikanController extends Controller
 {
     /**
@@ -11,7 +11,8 @@ class perbaikanController extends Controller
      */
     public function index()
     {
-        //
+        $perbaikan = Mperbaikan::get();
+        return view('perbaikan.index', compact('perbaikan')); 
     }
 
     /**
