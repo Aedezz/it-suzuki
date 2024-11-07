d61c56ea14f04389d4779c228a2e7dddb7107dea
Ardhana <firdausdepa.a@gmail.com>
2024-11-07 11:33:51 +0800

    update

--- database/migrations/2024_11_07_032115_perbaikan.php
+++ database/migrations/2024_11_07_032115_perbaikan.php
@@ -11,7 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('perbaikan', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('no_induk')->nullable();
+            $table->string('nama_leng')->nullable();
+            $table->string('jabatan')->nullable();
+            $table->string('divisi_cbg')->nullable();
+            $table->string('kode_ass')->nullable();
+            $table->string('tanggal')->nullable();
+            $table->string('nama_barang')->nullable();
+            $table->string('jumlah')->nullable();
+            $table->string('spesifikasi')->nullable();
+            $table->string('keluhan')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
