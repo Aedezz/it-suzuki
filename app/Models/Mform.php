dcc7fccb86938db9b6bc7e1606eb396e8cc5ec54
Ardhana <firdausdepa.a@gmail.com>
2024-11-07 14:37:22 +0800

    update

--- app/Models/Mform.php
+++ app/Models/Mform.php
@@ -9,5 +9,5 @@ class Mform extends Model
 {
     use HasFactory;
     protected $table = 'form_perangkat';
-    protected $fillable = ['nomor', 'nama_lengkap', 'jabatan', 'divisi_cabang', 'kode_asset', 'tgl_beli', 'nama_barang',  'jumlah', 'spesifikasi', 'keluhan'];
+    protected $fillable = ['nomor', 'nama_lengkap', 'jabatan', 'divisi_cabang', 'kode_asset', 'tgl_beli', 'nama_barang',  'jumlah', 'spesifikasi', 'keluhan', 'it', 'date_it', 'cek'];
 }
