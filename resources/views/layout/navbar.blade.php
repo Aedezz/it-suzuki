fd67377d3262593601be7930b9f1e21f9c6ce5be
alwikoord <alwikoord@gmail.com>
2024-11-08 16:40:10 +0800

    update icon

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>IT</title>
+    <title>IT Suzuki</title>
+    <link rel="icon" href="{{ asset('computing.png') }}" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
