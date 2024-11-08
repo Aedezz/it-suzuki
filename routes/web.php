586e8da24541d140053f9da2965f76e6b434f932
alwikoord <alwikoord@gmail.com>
2024-11-08 14:25:39 +0800

    route update

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ Route::get('/', function () {
 })->name('dashboard');
 
 // Routes for form input with CobaController
-Route::prefix('data-entry')->group(function () {
-    Route::get('/create', [CobaController::class, 'create'])->name('data-entry.create');
-    Route::post('/store', [CobaController::class, 'store'])->name('data-entry.store');
-});
+Route::get('/create', [CobaController::class, 'create'])->name('data-entry.create');
+Route::post('/store', [CobaController::class, 'store'])->name('data-entry.store');
 
 // Routes for guests (unauthenticated users)
 Route::middleware(['guest'])->group(function () {
