1d706813c6b3edc1b83b103676607e46d19e7930
Ardhana <firdausdepa.a@gmail.com>
2024-11-18 11:09:41 +0800

    new branch

--- app/Http/Controllers/perbaikanController.php
+++ app/Http/Controllers/perbaikanController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Mperbaikan;
 use Illuminate\Http\Request;
 
-class perbaikanController extends Controller
+class PerbaikanController extends Controller
 {
     public function create()
     {
