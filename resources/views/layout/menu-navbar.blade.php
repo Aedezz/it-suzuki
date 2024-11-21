30f43a382278baad9020eb8e04f466dd5a45a111
alwikoord <alwikoord@gmail.com>
2024-11-21 10:33:32 +0800

    Merge Period

--- resources/views/layout/menu-navbar.blade.php
+++ resources/views/layout/menu-navbar.blade.php
@@ -169,7 +169,7 @@
                     <i id="generalIcon" class="bi bi-caret-down-fill text-2xl caret-hover"></i>
                 </button>
                 <div id="generalDropdown" class="menuDropdown absolute bg-white text-gray-800 left-0 mt-2 w-48 rounded-lg shadow-lg">
-                    <a href="#" class="menu-link block px-4 py-2 hover:bg-gray-200">Period</a>
+                    <a href="{{route('periode.index')}}" class="menu-link block px-4 py-2 hover:bg-gray-200">Period</a>
                     <a href="{{route('home-activity')}}" class="menu-link block px-4 py-2 hover:bg-gray-200">Activity</a>
                     <a href="{{route('branch')}}" class="menu-link block px-4 py-2 hover:bg-gray-200">Branch</a>
                     <a href="{{ route('group.index')}}" class="menu-link block px-4 py-2 hover:bg-gray-200">Group</a>
