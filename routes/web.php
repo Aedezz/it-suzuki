71a2e7c0e8aa1f18ed774dd81a8ad378dc39bfdd
Muhammad Alwi <muhammadalwi@unida.gontor.ac.id>
2024-12-06 14:55:05 +0800

    Merge Report

--- routes/web.php
+++ routes/web.php
@@ -97,7 +97,7 @@ Route::get('/services/history/create', [HistoryPerbaikanController::class, 'crea
 Route::post('/services/history/store', [HistoryPerbaikanController::class, 'store'])->name('services.history.store');
 Route::get('/search-pegawai', [HistoryPerbaikanController::class, 'searchPegawai']);
 
-Route::get('/report/aktifitas', [ReportAktifitasController::class, 'index'])->name('aktifitas.index');
+Route::get('/report/aktifitas/v', [ReportAktifitasController::class, 'index'])->name('aktifitas.index');
 Route::get('/report/aktifitas', [ReportAktifitasController::class, 'previewReport'])->name('aktifitas.preview');
 ///////////////////////////
 
