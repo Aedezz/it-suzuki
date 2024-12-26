39796d62cd969ddd48372156d4e310cc2c3aceec
DhimasIX <543221147@smktelkom-bjb.sch.id>
2024-12-26 09:13:06 +0800

    Update table.blade.php

--- resources/views/problem/table.blade.php
+++ resources/views/problem/table.blade.php
@@ -1,12 +1,16 @@
 @forelse ($problems as $index => $problem)
-    <tr class="{{ $loop->even ? 'bg-gray-50' : 'bg-white' }}">
-        <td class="px-4 py-2 border-b">{{ $loop->iteration }}</td>
-        <td class="px-4 py-2 border-b">{{ $problem->kategori }}</td>
-        <td class="px-4 py-2 border-b">{{ \Carbon\Carbon::parse($problem->down)->format('d M Y H:i') }}</td>
-        <td class="px-4 py-2 border-b">{{ \Carbon\Carbon::parse($problem->up)->format('d M Y H:i') }}</td>
-        <td class="px-4 py-2 border-b">{{ $problem->pic }}</td>
-        <td class="px-4 py-2 border-b">{{ $problem->informasi }}</td>
-        <td class="px-4 py-2 border-b">{{ $problem->nama_cabang }}</td>
+    <tr class="{{ $loop->even ? 'bg-gray-50' : 'bg-white' }} hover:bg-gray-100">
+        <td class="px-4 py-2 border-b text-center">{{ $loop->iteration }}</td>
+        <td class="px-4 py-2 border-b text-center">{{ $problem->kategori ?? '-' }}</td>
+        <td class="px-4 py-2 border-b text-center">
+            {{ $problem->down ? \Carbon\Carbon::parse($problem->down)->format('d M Y H:i') : '-' }}
+        </td>
+        <td class="px-4 py-2 border-b text-center">
+            {{ $problem->up ? \Carbon\Carbon::parse($problem->up)->format('d M Y H:i') : '-' }}
+        </td>
+        <td class="px-4 py-2 border-b text-center">{{ $problem->pic ?? '-' }}</td>
+        <td class="px-4 py-2 border-b text-center">{{ $problem->informasi ?? '-' }}</td>
+        <td class="px-4 py-2 border-b text-center">{{ $problem->nama_cabang ?? '-' }}</td>
     </tr>
 @empty
     <tr>
